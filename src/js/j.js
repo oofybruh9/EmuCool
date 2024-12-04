c3ff8d56b9261cb457f54129c63ca6a17dd6e13e
oofybruh9 <oofybruh9@gmail.com>
2024-12-03 21:08:48 -0800

    grrrr

---
+++ src/js/j.js
@@ -0,0 +1,12 @@
+async function saveTS(json, place) {
+    let myjson = JSON.stringify(json);
+    await Neutralino.filesystem.writeFile('../configs/'+place+".json", myjson)
+};
+  
+async function loadFS(place) {
+    let myjson2 = await Neutralino.filesystem.readFile(place)
+    let json = JSON.parse(myjson2);
+    return json2;
+}
+
+
